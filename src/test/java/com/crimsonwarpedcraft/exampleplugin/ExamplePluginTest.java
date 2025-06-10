b0e3a2b10b079d47f733a522926164eee399e54f
JammyCat91283 <marktaoun@gmail.com>
2025-06-10 00:19:08 -0500

    Update ExamplePluginTest.java

--- src/test/java/com/crimsonwarpedcraft/exampleplugin/ExamplePluginTest.java
+++ src/test/java/com/crimsonwarpedcraft/exampleplugin/ExamplePluginTest.java
@@ -1,16 +1,1 @@
-package com.crimsonwarpedcraft.exampleplugin;
-
-import org.junit.jupiter.api.Test;
-
-/**
- * Tests for ExamplePlugin.
- *
- * @author Copyright (c) Levi Muniz. All Rights Reserved.
- */
-public class ExamplePluginTest {
-
-  @Test
-  public void onEnable() {
-
-  }
-}
+_DELETE_ME_
